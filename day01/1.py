a2a41123227e19160ac46b33907a699d4a94bbde
casey <casey@localhost.localdomain>
2017-12-24 11:26:03 +0800

    20171224: first study

---
+++ day01/1.py
@@ -0,0 +1,19 @@
+# python 版本
+
+# - 2.3
+# - 2.7
+# - 3.5
+# - 3.7
+
+print("Hello world!")
+print(123, "helloworld")
+# print(abcde)
+print("12345")
+print("*")
+print("**")
+print("***")
+print("****")
+# print()
+# print("  *")
+# print(" ***")
+# print("*****")
